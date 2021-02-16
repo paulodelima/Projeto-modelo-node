c8689c756f409328448087a0dbb4b5d2f6349722
Paulo de Lima <paulodelimagp@gmail.com>
2021-02-16 18:00:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Projeto-modelo-node
+ 
---
+++ main.js
@@ -0,0 +1,5 @@
+const variavelNumerica = 2;
+
+if (variavelNumerica == true && variavelNumerica == true && variavelNumerica == true && variavelNumerica == true && variavelNumerica == true && variavelNumerica == true && variavelNumerica == true && variavelNumerica == true) {
+    console.info('data');
+}
