dc7b9bddf9f1588dac0f26808acdf27b60c8b7e2
Paulo de Lima <paulodelimagp@gmail.com>
2021-02-16 21:26:01 -0400

    projeto padrão node

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Projeto-modelo-node
- 
+
 ## Projeto baseado nas orientações do projeto abaixo
 
 https://github.com/FabricaDeSinapse/fabrica-sinapse-nodejs-template
--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 const variavelNumerica = true;
 
-if (variavelNumerica == true && variavelNumerica == true && variavelNumerica == true && variavelNumerica == true && variavelNumerica == true && variavelNumerica == true && variavelNumerica == true && variavelNumerica == true) {
-        console.info('data');
+if (
+    variavelNumerica == true
+    && variavelNumerica == true
+    && variavelNumerica == true
+    && variavelNumerica == true
+    && variavelNumerica == true
+    && variavelNumerica == true
+    && variavelNumerica == true
+    && variavelNumerica == true
+) {
+    console.info('data');
 }
+
+const variavelNumerica = '2';
+  
+  if (variavelNumerica == 2 && 
+    variavelNumerica === 2) {
+             console.log('data');
+  }
